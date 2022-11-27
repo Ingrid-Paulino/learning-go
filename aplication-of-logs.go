f1895e36182159d41e4ad3278ba916d96979728f
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-11-27 16:08:18 -0300

    feat: lerning if in go

--- aplication-of-logs.go
+++ aplication-of-logs.go
@@ -15,9 +15,20 @@ func main() {
 	var comando int
 	//Capitura o que o usuario escreve
 	fmt.Scanf("%d", &comando)
-	fmt.Scan(&comando) //nao preciso passar o modificador
+	//ou
+	//fmt.Scan(&comando) //nao preciso passar o modificador
 	// %d sig que espero um inteiro
 	//& sig que oq o usuario digitar eu quero salvar na variavel comando
 	fmt.Println("O endereço da minha variavel comando é", &comando)
 	fmt.Println("O comando escolhido foi:", comando)
+
+	if comando == 1 { //No go condição sempre tem que retornar booleano (trur or false)
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa...")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
