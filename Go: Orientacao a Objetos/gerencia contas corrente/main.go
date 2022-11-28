37fe4f7003cd1de9071bc33e50d6f7b7457cbf7f
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-11-28 16:25:59 -0300

    feat: implements form of get money

--- Go: Orientacao a Objetos/gerencia contas corrente/main.go
+++ Go: Orientacao a Objetos/gerencia contas corrente/main.go
@@ -1,1 +1,34 @@
-package gerencia_contas_corrente
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+// (c *ContaCorrente): é como se fosse o "this", c vai seu o objeto da pessoa que estiver tentando sacar o dinheiro
+// c é quem esta chamando a função sacar
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func main() {
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Sacar(400)) // c na função sacar é contaDaSilva
+	fmt.Println(contaDaSilvia.saldo)
+
+}
