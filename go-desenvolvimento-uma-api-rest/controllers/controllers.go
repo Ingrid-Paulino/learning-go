87310cfe77662933e917cbaa96417f40fa78000a
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-11-30 13:26:09 -0300

    feat: add endpoint with method Post e Put

--- go-desenvolvimento-uma-api-rest/controllers/controllers.go
+++ go-desenvolvimento-uma-api-rest/controllers/controllers.go
@@ -33,3 +33,28 @@ func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&personalidade, id) // &p = busco o endereço de memoria, do atributo pelo id
 	json.NewEncoder(w).Encode(personalidade)
 }
+
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade) //pega uma imformacao no formato json e decodifica/converte para algo que o GO e a ORM entenda para salvar no BD
+	database.DB.Create(&novaPersonalidade)             //salva no banco de dados
+	json.NewEncoder(w).Encode(novaPersonalidade)       //exibe o dado em json
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // variavel que busca toda as varaveis
+	id := vars["id"]    // pega so pelo id
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewDecoder(r.Body).Decode(&personalidade) //pega o corpo da requisicao
+	database.DB.Save(&personalidade)               //salva no banco as mudancas
+	json.NewEncoder(w).Encode(personalidade)
+}
